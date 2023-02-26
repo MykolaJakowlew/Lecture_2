19559ce470407aa923f5ffab265bc04c19bad7be
chatbotbuilder <robotbuilder@chatbots.studio>
2023-02-26 19:21:36 +0200

    save result

---
+++ api/movies.api.js
@@ -0,0 +1,26 @@
+const { Router } = require('express');
+const { Movies } = require('../models/movies');
+
+const router = Router();
+
+router.get('/movies', async (req, res) => {
+ const queryDb = {};
+ const {
+  // Example: "Western,Drama"
+  genres,
+  // default 0
+  skip = 0,
+  // default 10
+  limit = 10
+ } = req.query;
+
+ if (genres) {
+  const parsedGenres = genres.split(','); // ["Western", "Drama"]
+  queryDb.genres = { $in: parsedGenres };
+ }
+
+ const movies = await Movies.find(queryDb).skip(skip).limit(limit);
+ return res.status(200).send(movies);
+});
+
+module.exports = { moviesAPiRouter: router };
