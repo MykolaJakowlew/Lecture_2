e2aa87d7478d03b2732dc74aa19e67c57165d68a
chatbotbuilder <robotbuilder@chatbots.studio>
2023-03-20 10:09:26 +0200

    save progress

--- server/telegram_bot.js
+++ server/telegram_bot.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require('telegraf');
-console.log(process.env.TELEGRAM_BOT_TOKEN)
-console.log(process.env.BASE_URL)
+console.debug(process.env.TELEGRAM_BOT_TOKEN);
+console.debug(process.env.BASE_URL);
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 module.exports = async (app, emitter) => {
@@ -9,30 +9,19 @@ module.exports = async (app, emitter) => {
  });
 
  bot.start((ctx) => {
-  // console.log('message:', ctx.message);
   ctx.reply('Welcome, use command "/login" to use your auth token\n\nExample: /login 12345-54645-564564...');
  });
 
  bot.command('login', (ctx) => {
-  // ctx.message.text => "/login 12345-54645-564564"
-  // option 1
-  // const id = ctx.message.text.slice(6);
-
-  // option 2
-  // const id = ctx.message.text.match(/[0-9a-z-]+$/ig);
-
-  // option 3
-  // const id = ctx.message.text.replace('/login', '').replaceAll(/ /, '');
-
-  // option 4
   const [command, id] = ctx.message.text.split(' ');
-  console.log(`Try to login id:${id}`);
+  const eventName = `login-${id}`;
+  console.log(`Try to login id:${id} event:<${eventName}>`);
   const userInfo = {
    firstName: ctx.from.first_name,
    lastName: ctx.from.last_name
   };
 
-  emitter.emit(`login-${id}`, userInfo);
+  emitter.emit(eventName, userInfo);
  });
 
  app.use(router);
--- server/websocket.js
+++ server/websocket.js
@@ -1,13 +1,23 @@
 const ws = require('ws');
-const SOCKET_PORT = 5000;
-const wss = new ws.Server({
- port: SOCKET_PORT
-}, () => {
- console.log(`Socket server started on ${SOCKET_PORT}`);
-});
+const wss = new ws.Server(
+ { port: process.env.SOCKET_PORT },
+ () => {
+  console.log(`Socket server started on ${process.env.SOCKET_PORT}`);
+ }
+);
+
+const sendToAll = (message) => {
+ wss.clients.forEach(ws => ws.send(JSON.stringify(message)));
+};
 
 wss.on('connection', (ws) => {
- ws.on('message', (message) => {
-
+ console.log(`New connection was created`);
+ ws.on('message', (messageString) => {
+  const message = JSON.parse(messageString);
+  console.log('message', message);
+  sendToAll(message);
+ });
+ ws.on('close', (reason) => {
+  console.log(`Connection was closed`, reason);
  });
 });
